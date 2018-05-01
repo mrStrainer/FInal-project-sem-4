303871d032218eb8c9c5f89e5d375da59f521b9c
Attila Szita <atee88@gmail.com>
2018-05-01 11:43:13 +0200

    get profile, kinda

--- Actions/Playlist.js
+++ Actions/Playlist.js
@@ -2,3 +2,12 @@
 export const fetchPlaylist = () => {
 	return 'nope'
 }
+
+export const fetchOwnPlaylists = (options) => {
+	return fetch(`https://api.spotify.com/v1/me/playlists`, options)
+		.then(json)
+		.then(status)
+		.then(res => {
+			return res;
+		})
+}
--- Actions/Profile.js
+++ Actions/Profile.js
@@ -1,3 +1,32 @@
-export const fetchProfile = (id = 'me') => {
-	return `Profile: ${id}`
+import { json, status, createHeader } from './Helpers'
+
+const REQUEST_PROFILE = 'REQUEST_OWN_PROFILE'
+const RECEIVE_PROFILE = 'RECEIVE_OWN_PROFILE'
+
+export const requestProfile = id => ({
+	type: REQUEST_PROFILE,
+	token
+})
+
+export const receiveProfile = profile => ({
+	type:RECEIVE_PROFILE,
+	profile
+})
+
+profileResponse = responseProfile => ({
+	name: responseProfile.display_name || responseProfile.id,
+	id:responseProfile.id,
+	followers:responseProfile.followers.total,
+	type:responseProfile.type,
+})
+export const fetchProfile = (id = 'me') => (dispatch, getState) => {
+	const endPoint = id === 'me' ? '/me' : `/users/${id}`
+	const { token } = getState().auth; 
+	dispatch(requestProfile(id));
+	return fetch(`https://api.spotify.com/v1${endpoint}`, createHeader('GET', token))
+		.then(json)
+		.then(status)
+		.then(profileResponse)
+		.then(profile => dispatch(receiveProfile(profile)))
+		.catch(error => console.log(error))
 }
